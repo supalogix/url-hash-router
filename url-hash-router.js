dd6db2587662e0c27600169e7adfe04c2b511fd4
Jonathan Nacionales <jnaciona@gmail.com>
2015-04-12 02:33:59 -0700

    Comment the code
    I want to optimize the code for human understanding. However, the
    current state of the code does not have this property. I have added some
    comments in a narrative format to help make the program more
    understandable.

--- url-hash-router.js
+++ url-hash-router.js
@@ -1,9 +1,16 @@
 var UrlHashRouter = (function(){
+	///
+	/// Create instance variables for this module so that the member 
+	/// functions can store state
+	///
 	function UrlHashRouter() {
 		this.routes = [];
 		this.unknownRoute = null;
 	}
 
+	///
+	/// Associate a regular expression with a callback and cache it
+	///
 	UrlHashRouter.prototype.register = function( regex, callback ) {
 		this.routes[ regex ] = callback;	
 	}
@@ -12,17 +19,29 @@ var UrlHashRouter = (function(){
 		this.unknownRoute = callback;
 	}
 
+	/// 
+	/// Execute a callback based on the location the caller provides.
+	/// Assume that the we only care about the first route we find. We do 
+	/// not care about conflicts with different routes (for now).
+	///
 	UrlHashRouter.prototype.execute = function( location ) {
 		var routes = this.routes;
 
+		///
+		/// Iterate through all the cached routes and key them off the 
+		/// regex patterns
+		///
 		for( var pattern in routes ) {
 			var callback = routes[pattern];
-			console.log(pattern);
 
 			var re = new RegExp(pattern) ;
 
 			var isTrue = re.test( location );
 
+			///
+			/// Skip this loop if the current location does not match the 
+			/// regex associated with this pattern
+			///
 			if( !isTrue )
 				continue;
 
@@ -31,16 +50,34 @@ var UrlHashRouter = (function(){
 			var args = getFunctionArguments( matches );
 
 			callback.apply(this, args);
+
+			///
+			/// Since we found a route we can terminate the loop
+			///
 			return;
 		}
 
+		///
+		/// We assume that there does not exist a route for the location if
+		/// the execution path reaches this point. Assume that the instance
+		/// variable unkownRoute represents the callback the caller wishes
+		/// to call.
+		///
 		if( this.unknownRoute !== null ) 
 			this.unknownRoute();
 	}
 
+	///
+	/// The results of a regular expression contain more the matching
+	/// patterns. Use this function to parse the results of a regex
+	/// match, and return just the matching patterns.
+	///
 	function getFunctionArguments( matches ) {
 		var args = [];
 
+		///
+		/// Return an empty array when there are no matches
+		///
 		if( !matches )
 			return args;
 
